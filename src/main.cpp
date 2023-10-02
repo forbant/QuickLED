7dbe83922557db29f992526374473ef0aad814e4
Andrii Tiutiakh <andrii.tiutiakh@globallogic.com>
2023-10-02 20:20:09 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+#include "FastLED.h"
+
+#define NUM_LEDS 124
+#define DATA_PIN 3
+
+#define BUTTON_PIN 4
+
+#define FORWARD_PIN 8
+#define BACKWARD_PIN 9
+#define STOVE_PIN 10
+#define CHARGE_PIN 11
+
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+    pinMode(FORWARD_PIN, INPUT);
+    pinMode(BACKWARD_PIN, INPUT);
+    pinMode(STOVE_PIN, INPUT);
+    pinMode(CHARGE_PIN, INPUT);
+}
+
+void testColor(CRGB color) 
+{
+    for (size_t i = 0; i < NUM_LEDS; i++)
+    {
+        leds[i] = color;
+        FastLED.show();
+        delay(10);
+    }
+    delay(2000);
+    for (size_t i = 0; i < NUM_LEDS; i++)
+    {
+        leds[i] = CRGB::Black;
+        FastLED.show();
+        delay(10);
+    }
+    delay(1000);
+}
+
+void loop()
+{
+    testColor(CRGB::White);
+    testColor(CRGB::Red);
+    testColor(CRGB::Green);
+    testColor(CRGB::Blue);
+    testColor(CRGB::Yellow);
+    testColor(CRGB::Cyan);
+    testColor(CRGB::Magenta);
+}
